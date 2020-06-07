a9eb65c661aa2b46194cabedf1e73553eea40aea
Josh Baltar <joshbaltar@gmail.com>
2020-06-07 00:04:34 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/jbaltar.github.io/Pathfinding/precache-manifest.1def5048be2e0c49e6552a919905b937.js"
+  "/jnbaltar.github.io/Pathfinding/precache-manifest.586315d843dc21ea056373d5d80ffea1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jbaltar.github.io/Pathfinding/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jnbaltar.github.io/Pathfinding/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
