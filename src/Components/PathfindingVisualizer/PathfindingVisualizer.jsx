068e9ce5deb6f8b706c9d9f6a1c2f2e93229d737
Josh Baltar <joshbaltar@gmail.com>
2020-06-16 18:28:32 -0700

    always showing calculating bitmoji when searching

--- src/Components/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/Components/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -117,7 +117,7 @@ export default class PathfindingVisualizer extends Component {
       }
       setTimeout(() => {
         const node = visitedNodesInOrder[i];
-        if (hasSolution && i === 0) {
+        if (i === 0) {
           document.getElementById(`node-${node.row}-${node.col}`).className =
             "node node-calculating node-visited";
         } else if (!hasSolution && i === visitedNodesInOrder.length - 1) {
